9d10854a8edc568ca325a7cdad99517f089a3a17
Kartik Goyal <kaartik05@gmail.com>
2024-04-23 23:07:26 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test.js';
+import Form from './Form.js';
+
+import Header_learn from './component_learn/header.js';
+import Footer_learn from './component_learn/footer.js';
+import Main_Learn from './component_learn/main.js';
+// import Favourite_Color from './States/State.js';
+// import ParentComponent from './Props/ParentComponent.js';
+import Notification from './ClassComponents/ClassComponent_demo.js';
+import DepositWithdraw from './Hooks/Reducer.js';
+import ParentComponent from './Context/context.js';
+import Browsing from './Routing/Merge.js';
+import React,{useState} from 'react';
+
+import Task from './TODO/Component.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+  // <div>
+  //   <Test/>
+  //   {/* <Form/> */}
+  // </div>
+  
+  // )
+
+  
+
 }
 
 export default App;
